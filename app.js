bb87c6bbf219feb4241d61b4b8821aa3bd12c3e8
harshalxo <guptaharshal50@gmail.com>
2020-06-08 19:39:40 +0000

    add comments

--- app.js
+++ app.js
@@ -2,29 +2,25 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var Blog = require("./models/blog");
+var Comment = require("./models/comment");
 
 mongoose.connect("mongodb://localhost/blogify", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// schema setup
-var blogSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-var Blog = mongoose.model("Blog", blogSchema);
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
 	res.render("landing");
 });
 
+// blogs
 app.get("/blogs", function(req, res) {
 	Blog.find({}, function(err, allBlogs) {
 		if(err) {
 			console.log(err);
 		} else {
-			res.render("index", {blogs: allBlogs});		
+			res.render("blogs/index", {blogs: allBlogs});		
 		}
 	});
 });
@@ -44,15 +40,51 @@ app.post("/blogs", function(req, res) {
 });
 
 app.get("/blogs/new", function(req, res) {
-	res.render("new");
+	res.render("blogs/new");
 });
 
 app.get("/blogs/:id", function(req, res) {
+	Blog.findById(req.params.id).populate("comments").exec(function(err, foundBlog) {
+		if(err) {
+			console.log(err);
+		} else {
+			Blog.find({}, function(err, allBlogs) {
+				if(err) {
+					console.log(err);
+				} else {
+					res.render("blogs/show", {blog: foundBlog, blogs: allBlogs});		
+				}
+			});
+		}
+	});
+});
+
+// comments
+app.get("/blogs/:id/comments/new", function(req, res) {
 	Blog.findById(req.params.id, function(err, foundBlog) {
 		if(err) {
 			console.log(err);
 		} else {
-			res.render("show", {blogs: foundBlog});
+			res.render("comments/new", {blog: foundBlog});
+		}
+	});
+});
+
+app.post("/blogs/:id/comments", function(req, res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if(err) {
+			console.log(err);
+			res.redirect("/blogs");
+		} else {
+			Comment.create(req.body.comment, function(err, newComment) {
+				if(err) {
+					console.log(err);
+				} else {
+					foundBlog.comments.push(newComment);
+					foundBlog.save();
+					res.redirect("/blogs/" + foundBlog._id);
+				}
+			});
 		}
 	});
 });
