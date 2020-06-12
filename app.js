36c3973d8436f657211f2dd94bd5bcd7fd493ebd
harshalxo <guptaharshal50@gmail.com>
2020-06-12 08:28:58 +0000

    add user authentication

--- app.js
+++ app.js
@@ -2,13 +2,33 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var localStrategy = require("passport-local");
 var Blog = require("./models/blog");
 var Comment = require("./models/comment");
+var User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/blogify", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+// passport configuration
+app.use(require("express-session")({
+	secret: "dating is bs",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
 
 app.get("/", function(req, res) {
 	res.render("landing");
@@ -25,7 +45,7 @@ app.get("/blogs", function(req, res) {
 	});
 });
 
-app.post("/blogs", function(req, res) {
+app.post("/blogs", isLoggedIn, function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var description = req.body.description;
@@ -39,7 +59,7 @@ app.post("/blogs", function(req, res) {
 	});
 });
 
-app.get("/blogs/new", function(req, res) {
+app.get("/blogs/new", isLoggedIn, function(req, res) {
 	res.render("blogs/new");
 });
 
@@ -60,7 +80,7 @@ app.get("/blogs/:id", function(req, res) {
 });
 
 // comments
-app.get("/blogs/:id/comments/new", function(req, res) {
+app.get("/blogs/:id/comments/new", isLoggedIn, function(req, res) {
 	Blog.findById(req.params.id, function(err, foundBlog) {
 		if(err) {
 			console.log(err);
@@ -70,7 +90,7 @@ app.get("/blogs/:id/comments/new", function(req, res) {
 	});
 });
 
-app.post("/blogs/:id/comments", function(req, res) {
+app.post("/blogs/:id/comments", isLoggedIn, function(req, res) {
 	Blog.findById(req.params.id, function(err, foundBlog) {
 		if(err) {
 			console.log(err);
@@ -88,6 +108,43 @@ app.post("/blogs/:id/comments", function(req, res) {
 		}
 	});
 });
+
+// auth routes
+app.get("/register", function(req, res) {
+	res.render("register");
+});
+app.post("/register", function(req, res) {
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user) {
+		if(err) {
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect("/blogs");
+		});
+	});
+});
+
+app.get("/login", function(req, res) {
+	res.render("login");
+});
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/blogs",
+	failureRedirect: "/login"
+}), function(req, res) {});
+
+app.get("/logout", function(req, res) {
+	req.logout();
+	res.redirect("/blogs");
+});
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect("/login");
+}
 
 app.listen(3000, function() {
 	console.log("Blogify server started!");
